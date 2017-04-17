64a5c279c1d716fab1d15a1eb7e77620936ca533
Vinny Martinez <vinny@vinnymartinez.com>
2017-04-17 10:29:58 -0400

    finished chapter 03

---
+++ 03_functions.js
@@ -0,0 +1,39 @@
+// Minimum
+function min(num1, num2) {
+  if (num1 < num2) return num1;
+  else if (num1 > num2) return num2;
+  else return 'numbers are the same';
+}
+
+console.log(min(1, 1));
+console.log(min(2, 1));
+console.log(min(-10, 1));
+
+// Recursion
+function isEven(num) {
+  if (num == 0) return true;
+  else if (num == 1) return false;
+  else if (num < 0) return isEven(num + 2);
+  else return isEven(num - 2);
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+
+// Bean Counting
+function countBs(string) {
+  return countChar(string, 'B');
+}
+
+console.log(countBs('ABBY'));
+console.log(countBs('VINNY'));
+
+function countChar(string, char) {
+  var charCount = 0;
+  for (var i = 0; i < string.length; i++) if (string.charAt(i) == char) charCount++;
+  return charCount
+}
+
+console.log(countChar('VINNY', 'N'));
+console.log(countChar('VINNY', 'Y'));
